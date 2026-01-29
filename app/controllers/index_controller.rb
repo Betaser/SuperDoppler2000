633368c0352d2a7df1d32de1d193a6a845a8eaef
Betaser <tylerharrisonhsieh@gmail.com>
2026-01-29 12:47:49 -0500

    Updating manually changed local files of this project to the remote repo

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,5 @@
+class IndexController < ApplicationController
+  def index
+    puts "root index"
+  end
+end
---
+++ app/controllers/subpage1_controller.rb
@@ -0,0 +1,40 @@
+require "matplotlib/pyplot"
+
+class Subpage1Controller < ApplicationController
+  def new
+    # just testing pyplot
+    plt = Matplotlib::Pyplot
+    xs = [ *1..100 ].map { |x| (x - 50) * Math::PI / 100.0 }
+    ys = xs.map { |x| Math.sin(x) }
+
+    plt.plot(xs, ys)
+    plt.show
+
+    @container_width = 500
+    @container_height = 350
+    @imgs = [
+      Img.new("coast.jpg", 600, 400, @container_width),
+      Img.new("lights.jpg", 600, 278, @container_width),
+      Img.new("forest.jpg", 300, 400, @container_width),
+      Img.new("mountains.jpg", 299, 400, @container_width)
+    ]
+  end
+
+  def go_back
+    puts "todo"
+    # redirect_to "/index/index"
+  end
+end
+
+class Img
+  attr_accessor :path
+  attr_accessor :width
+  attr_accessor :height
+
+  def initialize(path, width, height, container_width)
+    @path = path
+    @width = container_width * 0.9
+    ratio = @width.to_f / width
+    @height = height * ratio
+  end
+end
