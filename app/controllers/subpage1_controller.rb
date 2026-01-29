ff75a8ce667ccac672a763a279f27a3357363ba9
Betaser <tylerharrisonhsieh@gmail.com>
2026-01-29 12:57:24 -0500

    Fixed up some temporary breaking matplotlib testing

--- app/controllers/subpage1_controller.rb
+++ app/controllers/subpage1_controller.rb
@@ -1,15 +1,5 @@
-require "matplotlib/pyplot"
-
 class Subpage1Controller < ApplicationController
   def new
-    # just testing pyplot
-    plt = Matplotlib::Pyplot
-    xs = [ *1..100 ].map { |x| (x - 50) * Math::PI / 100.0 }
-    ys = xs.map { |x| Math.sin(x) }
-
-    plt.plot(xs, ys)
-    plt.show
-
     @container_width = 500
     @container_height = 350
     @imgs = [
