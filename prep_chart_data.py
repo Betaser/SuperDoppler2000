b32b7cefd63aae9f2e83dd3f06c705ee5d8559cd
Betaser <tylerharrisonhsieh@gmail.com>
2026-04-21 17:07:50 -0400

    debugging prep file done

--- prep_chart_data.py
+++ prep_chart_data.py
@@ -1,16 +1,20 @@
 import time
 import random
 
+def mk_row():
+    return f"{random.uniform(0, 10)},{random.uniform(30, 50)},{random.uniform(0, 0.5)},{random.uniform(0, 360)}"
+
 def update(data):
     data.pop(0)
-    data.append(random.uniform(0, 10))
+    data.append(mk_row())
 
-data = [random.uniform(0, 10) for _ in range(10)]
+data = [mk_row() for _ in range(10)]
+
 while True:
     time.sleep(1)
     update(data)
     with open("./ruby_on_rails_app/app/assets/chart_data.txt", "w") as f:
         data_str = ""
-        for item in data:
-            data_str += f"{item}\n"
+        for row in data:
+            data_str += f"{row}\n"
         f.write(data_str)
